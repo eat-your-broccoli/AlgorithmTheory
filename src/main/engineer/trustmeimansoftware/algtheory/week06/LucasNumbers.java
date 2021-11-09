b3f2e0d0eaa0b42804cb70e6acf369a539e6d2f1
Luca Göttle <l-goettle@gmx.de>
2021-11-09 19:27:29 +0100

    wip

--- src/main/engineer/trustmeimansoftware/algtheory/week06/LucasNumbers.java
+++ src/main/engineer/trustmeimansoftware/algtheory/week06/LucasNumbers.java
@@ -1,25 +1,46 @@
 package engineer.trustmeimansoftware.algtheory.week06;
 
 import java.math.BigInteger;
-import java.util.Arrays;
+import java.util.HashMap;
 
 public class LucasNumbers {
-    public static BigInteger L(int n, BigInteger[] memory) {
-        if(!memory[n].equals(BigInteger.ZERO)) return memory[n];
+
+    static HashMap<String, BigInteger> lMemory = new HashMap<>();
+    static HashMap<String, BigInteger> aMemory = new HashMap<>();
+
+    public static BigInteger A(BigInteger n) {
+        if(aMemory.containsKey(n.toString())) return aMemory.get(n.toString());
+        BigInteger modOp = new BigInteger("9876543210");
+        aMemory.put(n.toString(), Lmod(L(n), modOp).mod(modOp));
+        return aMemory.get(n.toString());
+    }
+
+    public static BigInteger L(BigInteger n) {
+        if(lMemory.containsKey(n.toString())) return lMemory.get(n.toString());
         BigInteger nBigInt =new BigInteger(String.valueOf(n));
-        if(n <= 1) memory[n] = BigInteger.TWO.subtract(nBigInt);
-        else memory[n] = L(n-1, memory).add(L(n-2, memory));
-        return memory[n];
+        if(n.compareTo(BigInteger.ONE) <= 0) lMemory.put(n.toString(), BigInteger.TWO.subtract(nBigInt));
+        else {
+            BigInteger res = L(n.subtract(BigInteger.ONE)).add(L(n.subtract(BigInteger.TWO)));
+            lMemory.put(n.toString(), res);
+        }
+        return lMemory.get(n.toString());
     }
 
-    public static BigInteger[] LArray(int n) {
-        BigInteger[] memory = new BigInteger[n+1];
-        Arrays.fill(memory, BigInteger.ZERO);
-        L(n, memory);
-        return memory;
+    public static BigInteger Lmod(BigInteger n, BigInteger modOperator) {
+        if(lMemory.containsKey(n.toString())) return lMemory.get(n.toString());
+        BigInteger nBigInt =new BigInteger(String.valueOf(n));
+        if(n.compareTo(BigInteger.ONE) <= 0) lMemory.put(n.toString(), BigInteger.TWO.subtract(nBigInt));
+        else {
+            BigInteger n1 = Lmod(n.subtract(BigInteger.ONE), modOperator);
+            BigInteger n2 = Lmod(n.subtract(BigInteger.TWO), modOperator);
+            BigInteger res = n1.add(n2).mod(modOperator);
+            lMemory.put(n.toString(), res);
+        }
+        return lMemory.get(n.toString());
     }
 
-    public static BigInteger L(int n) {
-        return LArray(n)[n];
+    public static HashMap<String, BigInteger> LArray(BigInteger n) {
+        L(n);
+        return lMemory;
     }
 }
--- src/test/engineer/trustmeimansoftware/algtheory/week06/LucasNumbersTest.java
+++ src/test/engineer/trustmeimansoftware/algtheory/week06/LucasNumbersTest.java
@@ -4,21 +4,43 @@ import org.junit.jupiter.api.Test;
 
 import java.math.BigInteger;
 import java.util.Arrays;
+import java.util.HashMap;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class LucasNumbersTest {
     @Test
     public void table0to100() {
-        BigInteger[] table = LucasNumbers.LArray(100);
-        for(int i = 0; i < table.length; i++) {
-            System.out.println(i+": "+table[i]);
+        HashMap<String, BigInteger> table = LucasNumbers.LArray(new BigInteger("100"));
+        for(int i = 0; i < table.size(); i++) {
+            System.out.println(i+": "+table.get(i+""));
         }
     }
 
     @Test
     public void lucasNum4321() {
-        System.out.println(LucasNumbers.L(4321));
+        System.out.println(LucasNumbers.L(new BigInteger("4321")));
     }
 
+    @Test
+    public void Lto100() {
+        HashMap<String, BigInteger> table = LucasNumbers.LArray(new BigInteger("100"));
+        for(int i = 0; i < table.size(); i++) {
+            System.out.println(i+": "+table.get(i+""));
+        }
+    }
+
+    @Test
+    public void ATable0to100() {
+        for(int i = 0; i <= 100; i++) {
+            System.out.println(i+": "+LucasNumbers.A(new BigInteger(i+"")));
+        }
+    }
+
+
+    @Test
+    public void test() {
+        int i = 20;
+        System.out.println(i+": "+LucasNumbers.A(new BigInteger(i+"")));
+    }
 }
